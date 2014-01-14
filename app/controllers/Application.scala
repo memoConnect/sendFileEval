371280d1f9cf3e9710c13391d5de739588b27e1f
Reimerei <git@bjrm.de>
2014-01-14 16:12:13 +0100

    added /api/sendFile

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,23 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import java.io.File
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("moin")
+  }
+
+  def sendFile = Action(parse.temporaryFile) {
+    request => {
+
+      val file: File = new File("/tmp/test")
+
+      request.body.copy(file)
+
+      Ok("ok")
+    }
   }
 
 }
